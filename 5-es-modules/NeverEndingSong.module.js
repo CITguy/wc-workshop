8e3530ff565e5e09ff7ee31d2f18d5fd8459a9cd
Ryan Johnson <rhino.citguy@gmail.com>
2018-11-25 19:56:51 -0600

    import and modified from codepen project

---
+++ 5-es-modules/NeverEndingSong.module.js
@@ -0,0 +1,64 @@
+/*
+ * NOTE: 
+ *  Normally, you'd want to name this file with a .mjs extension, but browsers and web servers 
+ *  haven't caught up with recognizing .mjs as a valid JavaScript MIME type.
+ */
+
+// Same as <template></template>
+const VERSE = document.createElement('template');
+// Define template content
+VERSE.innerHTML = `
+  <style>
+    .verse {
+      font-family: monospace;
+      margin: 0 0 1em 1em;
+    }
+  </style>
+  <div class="verse">
+    <div class="line">This is the song that doesn't end</div>
+    <div class="line">Yes, it goes on and on my friend</div>
+    <div class="line">Some people started singing it, not knowing what it was</div>
+    <div class="line">And they'll continue singing it forever just because</div>
+  </div>
+`;
+
+class CustomElement extends HTMLElement {
+    constructor () {
+        super();
+        this.attachShadow({ mode: 'open' });
+        this.start();
+    }
+
+    newVerse () {
+        this.shadowRoot.appendChild(VERSE.content.cloneNode(true));
+    }
+
+    clear () {
+        this.shadowRoot.innerHTML = '';
+    }
+
+    restart () {
+        this.stop();
+        this.clear();
+        this.start();
+    }
+
+    start () {
+        this.newVerse();
+        this._interval = window.setInterval(this.newVerse.bind(this), 5000);
+    }
+
+    stop () {
+        window.clearInterval(this._interval);
+    }
+}
+
+function upgrade () {
+    customElements.define('never-ending-song', CustomElement);
+}
+
+// This is what we can import in our ES6 JavaScript
+export default {
+    CustomElement,
+    upgrade,
+}
