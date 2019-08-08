775f5f7ce67e41c08e5b91b553e92ef29760c20f
Ryan Johnson <rhino.citguy@gmail.com>
2019-08-07 22:00:41 -0500

    wip(*): 20190807

--- src/assets/NeverEndingSong.module.js
+++ src/assets/NeverEndingSong.module.js
@@ -5,7 +5,8 @@
  */
 
 // Same as <template></template>
-const VERSE = document.createElement('template');
+const VERSE = document.createElement('template')
+
 // Define template content
 VERSE.innerHTML = `
   <style>
@@ -20,41 +21,42 @@ VERSE.innerHTML = `
     <div class="line">Some people started singing it, not knowing what it was</div>
     <div class="line">And they'll continue singing it forever just because</div>
   </div>
-`;
+`
 
 class CustomElement extends HTMLElement {
     constructor () {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.start();
+        super()
+        this.attachShadow({ mode: 'open' })
+        this.start()
     }
 
     newVerse () {
-        this.shadowRoot.appendChild(VERSE.content.cloneNode(true));
+        let cloneVerse = VERSE.content.cloneNode(true)
+        this.shadowRoot.appendChild(cloneVerse)
     }
 
     clear () {
-        this.shadowRoot.innerHTML = '';
+        this.shadowRoot.innerHTML = ''
     }
 
     restart () {
-        this.stop();
-        this.clear();
-        this.start();
+        this.stop()
+        this.clear()
+        this.start()
     }
 
     start () {
-        this.newVerse();
-        this._interval = window.setInterval(this.newVerse.bind(this), 5000);
+        this.newVerse()
+        this._interval = window.setInterval(this.newVerse.bind(this), 5000)
     }
 
     stop () {
-        window.clearInterval(this._interval);
+        window.clearInterval(this._interval)
     }
 }
 
 function upgrade () {
-    customElements.define('never-ending-song', CustomElement);
+    customElements.define('never-ending-song', CustomElement)
 }
 
 // This is what we can import in our ES6 JavaScript
